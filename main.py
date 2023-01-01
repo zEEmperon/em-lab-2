27f5a78cf8f7d12e22a36885c53b5da975f2f7f3
zEEmperon <daniil.filippenkov@tentonteam.com>
2023-01-01 18:46:02 +0200

    generated dataset

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
-def load_csv(filename: str) -> pd.DataFrame:
-    return pd.read_csv(filename)
+import numpy as np
 
 
 def main():
-    filename = "data.csv"
-    df = load_csv(filename)
-
-    # histogram
-    plt.hist(df["Data"])
-    plt.title("Гістограма")
-    plt.show()
-
-    minValue = math.floor(df["Data"].min())
-    maxValue = math.ceil(df["Data"].max())
-
-    # Frequency table
-    freq_data = df.apply(pd.Series.value_counts, bins=[*range(minValue, maxValue + 1)])
-
-    print()
-    print("Frequency table")
-    print()
-    print(freq_data)
-
-    # Statistics summary
-    print()
-    print("Statistics summary")
-    print()
-    print("Mean:", df["Data"].mean())
-    print("Median:", df["Data"].median())
-    print("Quantile:", df["Data"].quantile(0.75))
+    Var = 16
+    loc = Var
+    scale = Var / 10
+    size = Var * 100
+    dataset = np.random.normal(loc, scale, size)
+    print(len(dataset))
 
 
 if __name__ == '__main__':
